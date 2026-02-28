1b24d1c17b15033b3f73e4f865269d3d703aab7d
Sebastian <dslozanorga@gmail.com>
2026-02-28 18:37:16 -0300

    Iniciando proyecto

---
+++ script/script.js
@@ -0,0 +1,16 @@
+const productos = [
+    {
+        id: "arturiaminifreak",
+        nombre: "Arturia Minifreak",
+        categoria: "Sinterizador",
+        precio: 1500,
+        imagen: "./media/syn-minifreak.png"
+    }
+
+
+
+
+
+
+
+]
